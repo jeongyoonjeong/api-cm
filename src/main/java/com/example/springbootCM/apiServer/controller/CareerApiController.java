450a20902cda9c46c0c6566058abe3fa5b16ff07
yunjeong <dbswjdwjd3535@gmail.com>
2021-05-17 19:07:46 +0900

    refactor: Change api design

--- src/main/java/com/example/springbootCM/apiServer/controller/CareerApiController.java
+++ src/main/java/com/example/springbootCM/apiServer/controller/CareerApiController.java
@@ -23,22 +23,21 @@ public class CareerApiController {
 //    private final ResponseService responseService;
 
     @ApiOperation(value = "인증자 address로, 커리어 정보 조회", notes = "인증자 addr과 매핑되는 커리어정보를 조회한다")
-    @GetMapping(value = "/careers/auth/{auth_addr}/emp")
+    @GetMapping(value = "/careers/auth/{auth_addr}")
     public List<Career> getCareersFromAuth(@PathVariable String auth_addr) {
         return careerService.findByAuth(auth_addr);
     }
 
 
     @ApiOperation(value = "근로자 address로, 커리어 정보 조회", notes = "근로자 addr과 매핑되는 커리어정보를 조회한다")
-    @GetMapping(value = "/careers/emp/{emp_addr}/auth")
+    @GetMapping(value = "/careers/emp/{emp_addr}")
     public List<Career> getCareersFromEmp(@PathVariable String emp_addr) {
         return careerService.findByEmp(emp_addr);
     }
 
     @ApiOperation(value = "커리어 정보 추가", notes = "커리어 정보를 추가한다")
-    @PostMapping(value = "career/emp/{emp_addr}")
+    @PostMapping(value = "career")
     public Career addCareerFromEmp(
-            @PathVariable String emp_addr,
             @RequestBody Career newCareer
     ) {
         return careerService.register(newCareer);
@@ -50,9 +49,8 @@ public class CareerApiController {
     }
 
     @ApiOperation(value = "커리어 db 정보 변경", notes = "커리어 db 정보를 변경한다.")
-    @PatchMapping(value = "career/emp/{emp_addr}")
+    @PatchMapping(value = "career")
     public Career updateCareerFromEmp(
-            @PathVariable String emp_addr,
             @RequestBody Career newCareer
     ) {
         return careerService.updateCareer(newCareer);
