6b1766545de748cb6b0228fbe67f916bc0e90b3f
yunjeong <dbswjdwjd3535@gmail.com>
2021-05-30 22:59:02 +0900

    fix: Fix security rule

--- src/main/java/com/example/springbootCM/Config/security/SecurityConfiguration.java
+++ src/main/java/com/example/springbootCM/Config/security/SecurityConfiguration.java
@@ -40,7 +40,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests() // 다음 리퀘스트에 대한 사용권한 체크
                 .antMatchers("/signin", "/signup").permitAll() // 가입 및 인증 주소는 누구나 접근가능
-                .antMatchers( "/v1/auth","/v1/career/**","/v1/careers/**").hasAnyRole("EMP","AUTH")
+                .antMatchers( "/v1/auth/**","/v1/career/**","/v1/careers/**").hasAnyRole("EMP","AUTH")
                 .anyRequest().hasAnyRole("EMP","AUTH") // 그외 나머지 요청은 모두 인증된 회원만 접근 가능
                 .and()
                 .cors()
